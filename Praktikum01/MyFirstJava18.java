9b933dd4fc2fef62e6841a8a2cdfff077a5b123a
ripkihebat <ripkeyajah@gmail.com>
2024-09-06 10:22:52 +0700

    Praktikum01

---
+++ Praktikum01/MyFirstJava18.java
@@ -0,0 +1,7 @@
+public class MyFirstJava18 {
+    public static void main(String[] args) {
+
+        System.out.println("Nama Saya Rifqi Hilmi Mubarok");
+        
+    }
+}
---
+++ Praktikum01/readme.md
@@ -0,0 +1,4 @@
+hallo GitHub, ini adalah repository pertama saya
+
+coba 1
+coba 2
