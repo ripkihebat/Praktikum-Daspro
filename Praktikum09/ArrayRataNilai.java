93c88550f6c4de1a926c4d3d573a78555563d966
ripkihebat <ripkeyajah@gmail.com>
2024-11-08 14:16:30 +0700

    Praktikum09

--- Praktikum09/ArrayRataNilai.java
+++ Praktikum09/ArrayRataNilai.java
@@ -4,18 +4,39 @@ public class ArrayRataNilai {
     public static void main(String[] args) {
         Scanner sc18 = new Scanner(System.in);
 
-        int[] nilaiMhs= new int[10];
-        double total = 0;
-        double rata2;
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int jumlahMhs = sc18.nextInt();
 
-        for (int i = 0; i < nilaiMhs.length; i++) {
+        int[] nilaiMhs= new int[jumlahMhs];
+        double totalLulus = 0, totalTidakLulus = 0;
+        int countLulus = 0, countTidakLulus = 0;
+        int nilaiTertinggi = 0;
+        int nilaiTerendah = 100;
+
+        for (int i = 0; i < jumlahMhs; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+" : ");
             nilaiMhs[i] = sc18.nextInt();
+            if (nilaiMhs[i] > 70) {
+                totalLulus += nilaiMhs[i];
+                countLulus++;
+            } else {
+                totalTidakLulus += nilaiMhs[i];
+                countTidakLulus++;
+            }
+            if (nilaiMhs[i] > nilaiTertinggi) {
+                nilaiTertinggi = nilaiMhs[i];
+            }
+            if (nilaiMhs[i] < nilaiTerendah) {
+                nilaiTerendah = nilaiMhs[i];
+            }
         }
-        for (int i = 0; i < nilaiMhs.length; i++) {
-            total += nilaiMhs[i];
-        }
-        rata2 = total/nilaiMhs.length;
-        System.out.println("Rata-rata nilai = "+rata2);
+
+        double rataLulus = countLulus > 0 ? totalLulus / countLulus : 0;
+        double rataTidakLulus = countTidakLulus > 0 ? totalTidakLulus / countTidakLulus : 0;
+
+        System.out.println("Rata-rata nilai lulus = "+rataLulus);
+        System.out.println("Rata-rata nilai tidak lulus = " + rataTidakLulus);
+        System.out.println("Nilai tertinggi = " + nilaiTertinggi);
+        System.out.println("Nilai terendah = " + nilaiTerendah);
     }
 }
