23da6ac8ac65d5075c7ad9167ec5df4434fb8229
ripkihebat <ripkeyajah@gmail.com>
2024-11-08 10:16:39 +0700

    Praktikum09

--- Praktikum09/ArrayNilai18.java
+++ Praktikum09/ArrayNilai18.java
@@ -5,12 +5,18 @@ public class ArrayNilai18 {
         Scanner sc18 = new Scanner(System.in);
         int[] nilaiAkhir = new int[10];
 
-        for (int i = 0; i < 10; i++) {
-            System.out.print("Masukkan nilai akhir ke-"+i+" :");
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            System.out.println("Masukkan nilai akhir ke-"+i+" : ");
             nilaiAkhir[i] = sc18.nextInt();
         }
-        for (int i = 0; i< 10; i++) {
-            System.out.println("Nilai akhir ke-"+i+" adalah"+nilaiAkhir[i]);
+
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            if (nilaiAkhir[i] > 70) {
+                System.out.println("Mahasiswa ke-" + i + " lulus!");
+            } else {
+                System.out.println("Mahasiswa ke-" + i + " tidak lulus!");
+            }
         }
+                    
     }
 }
