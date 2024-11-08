b13dec0daccef8021d38e985e00c969f5e3ea9af
ripkihebat <ripkeyajah@gmail.com>
2024-11-08 15:28:07 +0700

    Praktikum09

--- Praktikum09/SearchNilai18.java
+++ Praktikum09/SearchNilai18.java
@@ -2,18 +2,35 @@ import java.util.Scanner;
 
 public class SearchNilai18 {
     public static void main(String[] args) {
-        int[] arrNilai = {80, 85, 78, 96, 90, 82, 86};
-        int key = 90;
-        int hasil = 0;
+        Scanner sc18 = new Scanner(System.in);
+
+        System.out.print("Masukkan banyaknya nilai yang akan diinput: ");
+        int nilai = sc18.nextInt();
+
+        int[] arrNilai = new int[nilai];
+        
+        for (int i = 0; i < nilai; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + " : ");
+            arrNilai[i] = sc18.nextInt();
+        }
+
+        System.out.print("Masukkan nilai yang ingin dicari: ");
+        int nilaiDicari = sc18.nextInt();
+        int hasil = -1;
 
         for (int i = 0; i < arrNilai.length; i++) {
-            if (key == arrNilai[i]) {
+            if (nilaiDicari == arrNilai[i]) {
                 hasil = i;
                 break;
             }
         }
-        System.out.println();
-        System.out.println("Nilai "+key+" ketemu di indeks ke-"+hasil);
-        System.out.println();
+
+        if (hasil != -1) {
+            System.out.println("Nilai "+nilaiDicari+" ketemu, merupakan nilai mahasiswa ke-" + (hasil + 1));
+        }  else {
+            System.out.println("Nilai yang dicari tidak ditemukan");
+        }
+
+        sc18.close();
     }
 }
